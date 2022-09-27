aa842cb5def0c4cc3bf912b50201765f82bf0ab7
bbangsol <rlaekthf1114@naver.com>
2022-09-27 15:05:54 +0900

    vue3-webpack-template

---
+++ src/components/Fruit.vue
@@ -0,0 +1,19 @@
+<template>
+<li>{{name}}</li>  
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1{
+    color: red !important; 
+  }
+</style>
